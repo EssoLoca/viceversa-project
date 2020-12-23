fc26dac600a0637be699d2d2d768dd3cad236d2c
admin <admin>
2020-12-23 15:37:36 +0600

    Add words

--- viceversa/views.py
+++ viceversa/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	kol = len(user_text.split())
+	reverse_text = user_text[::-1]
+	return render(request, 'Reverse.html', {'usertext':user_text, 'kol':kol, 'rev_text': reverse_text})
